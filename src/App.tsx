5acb571e6ceddf10ada6af338298ed6441952aa2
gcannatellacodgo <gaetanocannatella@codgo.it>
2026-01-28 17:09:22 +0100

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
+import StaffLogin from "./pages/StaffLogin.tsx";
 
 
 
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/staff" element={<StaffLogin />} />
         </Routes>
     );
 }
